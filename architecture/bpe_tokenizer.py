9ea201a6f052ae210288e13c56320387f73025ef
twqb <wjldwdsj@163.com>
2026-09-08 23:31:56 +0800

    simple bpe tokenizer

--- architecture/bpe_tokenizer.py
+++ architecture/bpe_tokenizer.py
@@ -55,18 +55,23 @@ class BPETokenizer:
     def train(self, corpus, num_merges=10):
         vocab = self.build_vocab(corpus)
         for i in range(num_merges):
+            # 每个词计算相邻token的频率
             stats = self.get_stats(vocab)
             if not stats:
                 break
+            # 取最高频率的pair
             best_pair, frequency = stats.most_common(1)[0]
 
             self.merges.append(best_pair)
+            # 进行一次merge
             vocab = self.merge_pair(vocab, best_pair)
 
         tokens = set()
+        # 把每个词的token都提取出来，装进大集合
         for word_tokens in vocab:
             tokens.update(word_tokens)
 
+        # 按照字典序排序，将下标索引作为token id
         tokens.add("<UNK>")
         self.vocab = {
             token: idx
@@ -97,6 +102,10 @@ class BPETokenizer:
         return tokens
 
     def encode(self, text):
+        """
+        bpe train后得到 self.vocab
+        基于self.vocab 可针对一段文本进行tokenize
+        """
         words = text.split()
         tokens = []
         for word in words:
